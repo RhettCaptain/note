6a14f279c02ed0089d53cc9cf5d13b155f60eb1a
hong_yu <hong_yu@worksap.co.jp>
2018-08-10 09:21:16 +0800

    tmp

---
+++ markdown/stm-project-2018-solo/src/main/java/com/worksap/stm2017/model/Person.java
@@ -0,0 +1,15 @@
+package com.worksap.stm2017.model;
+
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private Integer personId;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+
+}
---
+++ markdown/stm-project-2018-solo/src/main/java/com/worksap/stm2017/model/Person2.java
@@ -0,0 +1,17 @@
+package com.worksap.stm2017.model;
+
+
+import lombok.Data;
+
+@Data
+public class Person2 {
+
+    private String personId;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    
+    
+    
+}
