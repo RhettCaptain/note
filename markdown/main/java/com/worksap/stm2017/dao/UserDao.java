53c1d4dd3f3fd8f36c50425eb3b2f199dd206fd6
rhettcaptain <425237277@qq.com>
2018-08-14 00:19:38 +0800

    tmp

--- markdown/main/java/com/worksap/stm2017/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.worksap.stm2017.dao;
-
-import com.worksap.stm2017.model.User;
-
-public interface UserDao {
-	public boolean checkAdmin(User user);
-	
-	public boolean checkEmpl(User user);
-}
--- markdown/main/java/com/worksap/stm2017/model/Login.java
+++
@@ -1,9 +0,0 @@
-package com.worksap.stm2017.model;
-
-import lombok.Data;
-
-@Data
-public class Login {
-	private Integer userId;
-	private String password;
-}
