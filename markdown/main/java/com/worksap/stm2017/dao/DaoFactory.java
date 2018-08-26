1af1965001cc1ee7061af8c3792d6ee97bc0c07c
rhettcaptain <425237277@qq.com>
2018-08-26 17:27:21 +0800

    solo ok

--- markdown/main/java/com/worksap/stm2017/dao/DaoFactory.java
+++
@@ -1,6 +0,0 @@
-package com.worksap.stm2017.dao;
-
-public interface DaoFactory {
-	public UserDao getUserDao();
-	public RosterDao getRosterDao();
-}
--- markdown/main/resources/static/js/header.js
+++
@@ -1,3 +0,0 @@
-document.writeln("<script src=\'https://code.jquery.com/jquery-3.2.1.min.js\'></script>");
-document.writeln("<link rel=\'stylesheet\' href=\'https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css\'>");
-document.writeln("<script src=\'https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js\'></script>");
